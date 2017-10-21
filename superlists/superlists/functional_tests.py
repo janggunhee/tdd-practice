35da1006bfa13d14abe06895a9ce2719c88f273c
janggunhee <jan9.9unhee@gmail.com>
2017-10-21 21:34:55 +0900

    Functional test now checks we can input a to-do item

--- superlists/superlists/functional_tests.py
+++ superlists/superlists/functional_tests.py
@@ -1,48 +1,66 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
-class NewVisitorTest(unittest.TestCase):  # 1
 
-    def setUp(self):  #2
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
         self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
 
-    def tearDown(self):   #3
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  #4
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # 에디스(Edith) 는 멋진 작업 목록 온라인 앱이 나왔다는 소식을 듣고
         # 해당 췝 사이트를 확인하러 간다.
         self.browser.get('http://localhost:8000')
 
-
         # 헤당 웹페이지 타이틀롸 헤더가 'To-Do'를 표시하고 있다.
-        self.assertIn('To-Do', self.browser.title)  #5
-        self.fail('Finish the test!')     #6
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # 그녀는 바로 작업을 추가하기로 한다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
 
+        # (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
+        inputbox.send_keys('Buy peacock feathers')
 
-# (에디스의 취미는 날치 잡이용 그물을 만드는 것이다)
-# 엔터키를 치면 페이지가 갱신되고 작업 목록에
-# "1: 공작깃털 사기" 아이템이 추가된다
+        # 엔터키를 치면 페이지가 갱신되고 작업 목록에
+        # "1: 공작깃털 사기" 아이템이 추가된다
+        inputbox.send_keys(Keys.ENTER)
 
-# 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
-# 다시 "공장깃털을 이용해서 그물 만들기" 라고 입력한다 (에디스는 매우 체계적인 사람이다)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('td')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
 
-# 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인ㄷ
-# 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
-# 사이트는 그녀를 위한 특정 URL을 생성해 준다
-# 이때 URL에 대한 설명도 함께 제공된다
+        # 추가 아이템을 입력할 수 있는 여분의 텍스트 상자가 존재한다.
+        # 다시 "공장깃털을 이용해서 그물 만들기" 라고 입력한다 (에디스는 매우 체계적인 사람이다)
 
-# 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
+        self.fail('Finish the test!')
 
-# 만족하고 잠자리에 든다.
+        # 페이지는 다시 갱신되고, 두 개 아이템이 목록에 보인ㄷ
+        # 에디스는 사이트가 입력한 목록을 저장하고 있는지 궁금하다
+        # 사이트는 그녀를 위한 특정 URL을 생성해 준다
+        # 이때 URL에 대한 설명도 함께 제공된다
+
+        # 해당 URL에 접속하면 그녀가 만든 작업 목록이 그대로 있는 것을 확인할 수 있다
+
+        # 만족하고 잠자리에 든다.
 
 
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
 
