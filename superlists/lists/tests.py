1d8a3dc88859bebac26e9fef09a2eb57dfc5a212
janggunhee <jan9.9unhee@gmail.com>
2017-10-21 22:36:32 +0900

    Refactor home page view to use a template2

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -10,4 +10,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
 
