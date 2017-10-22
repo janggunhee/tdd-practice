ba4caaf7083c32333e0666e8d9473b7debaf31b0
janggunhee <jan9.9unhee@gmail.com>
2017-10-22 09:33:42 +0900

    FT 리팩터

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,19 +1,50 @@
-from django.core.urlresolvers import resolve
+import re
+
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
+from django.urls import resolve
+
 from .views import home_page
 
+
 class HomePageTest(TestCase):
+    pattern_input_csrf = re.compile(r'<input[^>]*csrfmiddlewaretoken[^>]*>')
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        response = home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {
+                'new_item_text': 'A new list item',
+            }
+        )
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
+
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        self.assertEqual(
+            re.sub(self.pattern_input_csrf, '', response.content.decode()),
+            re.sub(self.pattern_input_csrf, '', expected_html)
+        )
 
 
